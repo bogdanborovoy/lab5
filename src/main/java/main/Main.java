6a1dbd4010593d75e9929b69e778f5ef940fb93c
bogdanborovoy <bogdanborovoy@icloud.com>
2025-04-08 20:49:04 +0300

    first commit

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,7 +11,6 @@ import com.opencsv.*;
 
 import java.io.*;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         //export FILE_NAME="/home/studs/s465267/lab5/files/values.csv" - helios
